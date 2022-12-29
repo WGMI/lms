0eaa439491f72e0359ae5f1d6b3202399474435b
WGMI <imo.muganda21@gmail.com>
2022-12-29 12:32:36 +0300

    First commit

---
+++ database/migrations/2022_12_28_064432_create_shelters_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSheltersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shelters', function (Blueprint $table) {
+            $table->id();
+            $table->integer('occupied')->default(0);
+            $table->unsignedBigInteger('animal_id')->nullable();
+            $table->foreign('animal_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shelters');
+    }
+}
